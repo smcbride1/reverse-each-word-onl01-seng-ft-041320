ae82ad5377237b4c27a6a3dca9f2cf50f257a22f
Stephen McBride <stevemcbride3@gmail.com>
2020-04-15 03:29:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   new_sentence = []
-  sentence_array.collect {| word | new_sentence.push((word.reverse))}.join(" ")
+  sentence_array.collect {| word | new_sentence.push(word.reverse)}
+  new_sentence.join(" ")
 end
